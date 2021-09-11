51ef5025c6041497f265a0e944f8b40e156a1391
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-09-12 04:30:38 +0900

    Add | Create music data model

---
+++ app/src/main/java/com/example/musicexoplayer/MusicModel.kt
@@ -0,0 +1,11 @@
+package com.example.musicexoplayer
+
+data class MusicModel(
+    val id: Long,
+    val track: String,
+    val streamUrl: String,
+    val artist: String,
+    val coverUrl: String,
+    val isPlaying: Boolean = false
+) {
+}
