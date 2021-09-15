09e31f315ff060168fe1f8ebef35eb6be129e346
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-09-15 21:58:28 +0900

    Add | Create PlayerModel

---
+++ app/src/main/java/com/example/musicexoplayer/PlayerModel.kt
@@ -0,0 +1,39 @@
+package com.example.musicexoplayer
+
+data class PlayerModel(
+    private val playerMusicList: List<MusicModel> = emptyList(),
+    var currentPosition: Int = -1,
+    var isWatchingPlayListView: Boolean = true
+) {
+
+    fun getAdapterModels(): List<MusicModel> {
+        return playerMusicList.mapIndexed{ index, musicModel ->
+            val newItem = musicModel.copy(
+                isPlaying = index == currentPosition
+            )
+            newItem
+        }
+    }
+
+    fun updateCurrentPosition(musicModel: MusicModel) {
+        currentPosition = playerMusicList.indexOf(musicModel)
+    }
+
+    fun nextMusic(): MusicModel? {
+        if (playerMusicList.isEmpty()) {
+            return null
+        }
+
+        currentPosition = if ((currentPosition + 1) == playerMusicList.size) 0 else currentPosition + 1
+        return playerMusicList[currentPosition]
+    }
+
+    fun prevMusic(): MusicModel? {
+        if (playerMusicList.isEmpty()) {
+            return null
+        }
+
+        currentPosition = if ((currentPosition - 1) < 0) playerMusicList.lastIndex else currentPosition - 1
+        return playerMusicList[currentPosition]
+    }
+}
